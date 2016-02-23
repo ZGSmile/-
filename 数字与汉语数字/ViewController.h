ec0519a4277e9c5e57afcb8c1ecbce26deda8ff3
guang <798476073@qq.com>
2016-02-22 23:19:21 -0800

    数字与汉语数字
    数字与汉语数字转化

---
+++ 数字与汉语数字/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  数字与汉语数字
+//
+//  Created by 祝争光 on 7/23/15.
+//  Copyright (c) 2015 com.zgteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 数字与汉语数字Tests/_______Tests.m
@@ -0,0 +1,40 @@
+//
+//  _______Tests.m
+//  数字与汉语数字Tests
+//
+//  Created by 祝争光 on 7/23/15.
+//  Copyright (c) 2015 com.zgteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _______Tests : XCTestCase
+
+@end
+
+@implementation _______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
